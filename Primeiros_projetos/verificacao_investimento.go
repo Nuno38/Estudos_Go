e6b417e022d2a0fe42dafe043a76f274cb3e3b8c
Elias Nascimento <70302321+Nuno38@users.noreply.github.com>
2025-03-21 22:18:40 -0300

    Feat
    Feat (The new):

--- Primeiros_projetos/verificacao_investimento.go
+++ Primeiros_projetos/verificacao_investimento.go
@@ -8,10 +8,11 @@ import (
 	"os"
 	"os/exec"
 	"runtime"
+	"strconv"
 	"time"
 )
 
-
+// Função para limpar a tela
 func clearScreen() {
 	var cmd *exec.Cmd
 	switch runtime.GOOS {
@@ -31,6 +32,7 @@ type Cotacao struct {
 	} `json:"USDBRL"`
 }
 
+
 func mensagemInicio() {
 	fmt.Println("\nBem-vindo ao sistema de cotação do dia!")
 	time.Sleep(3 * time.Second)
@@ -38,52 +40,62 @@ func mensagemInicio() {
 }
 
 
-func insecaoDeValores() (string, float64) {
-	var nomeFundo string
-	var valorFundo float64
-
-	fmt.Print("Insira o nome do Fundo: ")
-	fmt.Scanln(&nomeFundo)
-
-	fmt.Print("Insira o valor do Fundo: ")
-	fmt.Scanln(&valorFundo)
-
-	return nomeFundo, valorFundo
-}
-
-// Função para buscar a cotação do dólar
-func cotacaoDoDia() {
+func cotacaoDoDia() (float64, error) {
 	site := "https://economia.awesomeapi.com.br/json/last/USD-BRL"
 
-	// Fazendo a requisição HTTP
 	resp, err := http.Get(site)
 	if err != nil {
-		fmt.Println("Erro ao fazer a requisição:", err)
-		return
+		return 0, fmt.Errorf("erro ao fazer a requisição: %v", err)
 	}
-	defer resp.Body.Close() 
-
+	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println("Erro ao ler a resposta:", err)
-		return
+		return 0, fmt.Errorf("erro ao ler a resposta: %v", err)
 	}
-
 
 	var cotacao Cotacao
 	err = json.Unmarshal(body, &cotacao)
 	if err != nil {
-		fmt.Println("Erro ao decodificar JSON:", err)
+		return 0, fmt.Errorf("erro ao decodificar JSON: %v", err)
+	}
+
+	
+	valorDolar, err := strconv.ParseFloat(cotacao.USDBRL.Bid, 64)
+	if err != nil {
+		return 0, fmt.Errorf("erro ao converter a cotação: %v", err)
+	}
+
+	return valorDolar, nil
+}
+
+// Função para verificar o investimento e calcular o rendimento
+func verificaInvestimento() {
+	cotacao, err := cotacaoDoDia()
+	if err != nil {
+		fmt.Println(err)
 		return
 	}
 
+	fmt.Println("Cotação do dólar hoje: R$", cotacao)
 
-	fmt.Println("Cotação do dólar hoje: R$", cotacao.USDBRL.Bid)
+	var valorInvestido float64
+
+	fmt.Print("Informe o valor investido em reais: ")
+	_, err = fmt.Scanln(&valorInvestido)
+	if err != nil {
+		fmt.Println("Erro ao ler o valor investido:", err)
+		return
+	}
+
+	// Calcula o valor em dólares e o rendimento
+	valorEmDolares := valorInvestido / cotacao
+
+	fmt.Printf("\nCom um investimento de R$ %.2f e a cotação de R$ %.2f\n", valorInvestido, cotacao)
+	fmt.Printf("Você teria aproximadamente USD %.2f\n", valorEmDolares)
 }
 
 func main() {
 	mensagemInicio()
-	cotacaoDoDia()
-	
+	verificaInvestimento()
 }
